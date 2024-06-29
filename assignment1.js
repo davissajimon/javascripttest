8a89ed61c1385a920809b72aa1fb080c752ee382
davissajimon <davissajimon04@gmail.com>
2024-06-29 09:18:13 +0530

    3rd commit

---
+++ assignment1.js
@@ -0,0 +1,99 @@
+// numbers
+let height = 156;
+console.log(height);
+let weight = 25;
+console.log(weight);
+
+// Strings:
+let color = "red";
+console.log(color);
+let name = "Sid";
+console.log(name);
+
+
+// Booleans
+let bool= 5 > 6;
+console.log(bool);
+let num1=5;
+let num2=5;
+console.log(num1==num2);
+
+//BigInt
+let num3 = BigInt("123456789012345678901234567890");
+console.log(num3);
+
+//null
+let user=null;
+console.log(null);
+
+//undefined
+let userr=undefined;
+console.log(userr)
+
+//Symbol
+let id1=Symbol("id");
+let id2=Symbol("id");
+console.log(id1==id2);
+
+
+// Object:
+const person= {firstName:"John", lastName:"Lukose", age:19, eyeColor:"blue"};
+console.log(person);
+
+
+//Array
+array=["john",30,true,null,undefined]
+console.log(array);
+const cars = ["suzuki", "Volvo", "BMW"];
+console.log(cars);
+
+//Assignment operator
+let r = 10;
+console.log(r);
+
+// Arithmetic operators
+//addition
+let x = 8;
+let y = 2;
+let z = x + y;
+console.log(z);
+//multiplication
+let w = x * y;
+console.log(w);
+// division
+let div = x/y;
+console.log(div);
+//increment
+let inc = 5;
+inc++;
+
+let res = inc;
+console.log(res);
+//decrement
+
+let dec = 5;
+dec--;
+let result = dec;
+console.log(result);
+//exponential
+let no = 5;
+let fin = no ** 2;
+console.log(fin);
+//addition assignment operator in text 
+let text = "Hello"; text += " World";
+console.log(text);
+// subtraction assignment operator
+let we = 10;
+we -= 5;
+console.log(we);
+//and logical operator
+let and = 10;
+and &&= 5;
+console.log(and);
+//or logical operator
+let or = 10;
+or ||= 5;
+console.log(or);
+
+
+
--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 console.log("Hello world");
-var a = 10;
-console.log(a);
-var a = "name";
-console.log(a);
-let name = "john";
-console.log(name);
-const pi = 3.14;
-console.log(pi);
-let value = 1;
-let value2 = "1";
-console.log(value + value2);
-console.log(value + parseInt(value2));
-let value3 = 1;
-let value4 = "a";
-console.log(value3 + value4);
-console.log(value3 + parseInt(value4));
-const dru = "john";
+const name = "john";
 const age = 30;
 const rating = 4.5;
-const isCool = true;
-console.log(typeof dru);
-
-
+const isCool = true
+arrayOne = ["john",30,true,null,undefined]
+console.log(arrayOne);
