3bcef881b683cd6046fbfc1c8ccbf3c511286686
davissajimon <davissajimon04@gmail.com>
2024-06-29 10:20:09 +0530

    new commit

--- script.js
+++ script.js
@@ -5,3 +5,90 @@ const rating = 4.5;
 const isCool = true
 arrayOne = ["john",30,true,null,undefined]
 console.log(arrayOne);
+console.log("Function");
+
+function showSting() {
+    console.log("Hello World");
+}
+
+showSting();
+
+var showString = function () {
+    console.log("Hello World");
+}
+
+showSting();
+
+
+var showSting = () => {
+    console.log(" Arrow Function Hello World");
+}
+
+showSting();
+
+
+// Map
+
+var number = [1, 2, 3, 4, 5];
+
+
+var square = number.map((num) => {
+    return num * num;
+})
+
+console.log(square);
+
+var fruits = ["Apple", "Banana", "Mango"];
+
+var upperCase = fruits.map((fruit) => {
+    return fruit.toUpperCase();
+});
+
+console.log(upperCase);
+
+// Enhanced Object Literals
+
+var name = "John";
+var age = 20;
+
+var person = {
+    name,
+    age,
+    isStudent: true
+}
+
+console.log(person);
+
+// Destructuring
+
+var person = {
+    name: "Akhil",
+    age: 20,
+    isStudent: true
+}
+var { name, age, isStudent } = person;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// promises
+
+var promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved")
+    }, 2000);
+})
+
+promise.then((reslut) => {
+    console.log(reslut);
+})
+
+
+// ! Spread Operator
+
+var numbers = [1, 2, 3, 4, 5];
+
+var newNumber = [...numbers, 6, 7, 8];
+
+console.log(newNumber);
